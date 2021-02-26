82565d59c72eac9b383bf4a86524fd2b3ef1c596
ValentinaKozlova <v.kozlova13@gmail.com>
2021-02-26 19:15:33 +0200

    Task 3: gallery

--- src/components/launch.js
+++ src/components/launch.js
@@ -19,10 +19,14 @@ import {
   Stack,
   AspectRatioBox,
   StatGroup,
+  ModalOverlay,
+  Modal
 } from "@chakra-ui/core";
 import { useDisclosure } from "@chakra-ui/react";
-import {AddToFavoritesButton} from "./add-to-favorites-button";
+import { ContentCentered } from "./styles/styles"
+import styled from "@emotion/styled";
 
+import {AddToFavoritesButton} from "./add-to-favorites-button";
 import { useSpaceX, useSpaceXPaginated } from "../utils/use-space-x";
 import { formatDateTime } from "../utils/format-date";
 import Error from "./error";
@@ -30,6 +34,21 @@ import Breadcrumbs from "./breadcrumbs";
 import {addToFavorites, getFavorites, removeFromFavorites} from "./updateFavorites";
 import FavoritesDrawer from "./favorites-drawer";
 import {LaunchItem} from "./launches";
+
+const ImageDiv = styled.div`
+  width: 416px;
+  height: 270px;
+  background: url(${props =>
+    props.bcdUrl || 'https://live.staticflickr.com/65535/50618376671_ba4e60af7c_z.jpg'});
+`;
+
+const OpenedImage = styled.div`
+    position: fixed;
+    background: url(${props =>
+      props.bcdUrl || 'https://live.staticflickr.com/65535/50618376671_ba4e60af7c_z.jpg'});
+      width: 1280px;
+      height: 100%;
+`
 
 export default function Launch() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -71,9 +90,11 @@ export default function Launch() {
       <Box m={[3, 6]}>
         <TimeAndLocation launch={launch} />
         <RocketInfo launch={launch} />
-        <Text color="gray.700" fontSize={["md", null, "lg"]} my="8">
-          {launch.details}
-        </Text>
+        <ContentCentered>
+          <Text color="gray.700" fontSize={["md", null, "lg"]} my="8">
+            {launch.details}
+          </Text>
+        </ContentCentered>
         <Video launch={launch} />
         <Gallery images={launch.links.flickr_images} />
       </Box>
@@ -162,37 +183,39 @@ function Header({ launch, onOpen, favoriteName, groupName }) {
 
 function TimeAndLocation({ launch }) {
   return (
-    <SimpleGrid columns={[1, 1, 2]} borderWidth="1px" p="4" borderRadius="md">
-      <Stat>
-        <StatLabel display="flex">
-          <Box as={Watch} width="1em" />{" "}
-          <Box ml="2" as="span">
-            Launch Date
-          </Box>
-        </StatLabel>
-        <StatNumber fontSize={["md", "xl"]}>
-          {formatDateTime(launch.launch_date_local)}
-        </StatNumber>
-        <StatHelpText>{timeAgo(launch.launch_date_utc)}</StatHelpText>
-      </Stat>
-      <Stat>
-        <StatLabel display="flex">
-          <Box as={MapPin} width="1em" />{" "}
-          <Box ml="2" as="span">
-            Launch Site
-          </Box>
-        </StatLabel>
-        <StatNumber fontSize={["md", "xl"]}>
-          <Link
-            as={RouterLink}
-            to={`/launch-pads/${launch.launch_site.site_id}`}
-          >
-            {launch.launch_site.site_name_long}
-          </Link>
-        </StatNumber>
-        <StatHelpText>{launch.launch_site.site_name}</StatHelpText>
-      </Stat>
-    </SimpleGrid>
+    <ContentCentered>
+      <SimpleGrid columns={[1, 1, 2]} borderWidth="1px" p="4" borderRadius="md">
+        <Stat>
+          <StatLabel display="flex">
+            <Box as={Watch} width="1em" />{" "}
+            <Box ml="2" as="span">
+              Launch Date
+            </Box>
+          </StatLabel>
+          <StatNumber fontSize={["md", "xl"]}>
+            {formatDateTime(launch.launch_date_local)}
+          </StatNumber>
+          <StatHelpText>{timeAgo(launch.launch_date_utc)}</StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel display="flex">
+            <Box as={MapPin} width="1em" />{" "}
+            <Box ml="2" as="span">
+              Launch Site
+            </Box>
+          </StatLabel>
+          <StatNumber fontSize={["md", "xl"]}>
+            <Link
+              as={RouterLink}
+              to={`/launch-pads/${launch.launch_site.site_id}`}
+            >
+              {launch.launch_site.site_name_long}
+            </Link>
+          </StatNumber>
+          <StatHelpText>{launch.launch_site.site_name}</StatHelpText>
+        </Stat>
+      </SimpleGrid>
+    </ContentCentered>
   );
 }
 
@@ -200,87 +223,103 @@ function RocketInfo({ launch }) {
   const cores = launch.rocket.first_stage.cores;
 
   return (
-    <SimpleGrid
-      columns={[1, 1, 2]}
-      borderWidth="1px"
-      mt="4"
-      p="4"
-      borderRadius="md"
-    >
-      <Stat>
-        <StatLabel display="flex">
-          <Box as={Navigation} width="1em" />{" "}
-          <Box ml="2" as="span">
-            Rocket
-          </Box>
-        </StatLabel>
-        <StatNumber fontSize={["md", "xl"]}>
-          {launch.rocket.rocket_name}
-        </StatNumber>
-        <StatHelpText>{launch.rocket.rocket_type}</StatHelpText>
-      </Stat>
-      <StatGroup>
+    <ContentCentered>
+      <SimpleGrid
+        columns={[1, 1, 2]}
+        borderWidth="1px"
+        mt="4"
+        p="4"
+        borderRadius="md"
+      >
         <Stat>
           <StatLabel display="flex">
-            <Box as={Layers} width="1em" />{" "}
+            <Box as={Navigation} width="1em" />{" "}
             <Box ml="2" as="span">
-              First Stage
+              Rocket
             </Box>
           </StatLabel>
           <StatNumber fontSize={["md", "xl"]}>
-            {cores.map((core) => core.core_serial).join(", ")}
+            {launch.rocket.rocket_name}
           </StatNumber>
-          <StatHelpText>
-            {cores.every((core) => core.land_success)
-              ? cores.length === 1
-                ? "Recovered"
-                : "All recovered"
-              : "Lost"}
-          </StatHelpText>
+          <StatHelpText>{launch.rocket.rocket_type}</StatHelpText>
         </Stat>
-        <Stat>
-          <StatLabel display="flex">
-            <Box as={Layers} width="1em" />{" "}
-            <Box ml="2" as="span">
-              Second Stage
-            </Box>
-          </StatLabel>
-          <StatNumber fontSize={["md", "xl"]}>
-            Block {launch.rocket.second_stage.block}
-          </StatNumber>
-          <StatHelpText>
-            Payload:{" "}
-            {launch.rocket.second_stage.payloads
-              .map((payload) => payload.payload_type)
-              .join(", ")}
-          </StatHelpText>
-        </Stat>
-      </StatGroup>
-    </SimpleGrid>
+        <StatGroup>
+          <Stat>
+            <StatLabel display="flex">
+              <Box as={Layers} width="1em" />{" "}
+              <Box ml="2" as="span">
+                First Stage
+              </Box>
+            </StatLabel>
+            <StatNumber fontSize={["md", "xl"]}>
+              {cores.map((core) => core.core_serial).join(", ")}
+            </StatNumber>
+            <StatHelpText>
+              {cores.every((core) => core.land_success)
+                ? cores.length === 1
+                  ? "Recovered"
+                  : "All recovered"
+                : "Lost"}
+            </StatHelpText>
+          </Stat>
+          <Stat>
+            <StatLabel display="flex">
+              <Box as={Layers} width="1em" />{" "}
+              <Box ml="2" as="span">
+                Second Stage
+              </Box>
+            </StatLabel>
+            <StatNumber fontSize={["md", "xl"]}>
+              Block {launch.rocket.second_stage.block}
+            </StatNumber>
+            <StatHelpText>
+              Payload:{" "}
+              {launch.rocket.second_stage.payloads
+                .map((payload) => payload.payload_type)
+                .join(", ")}
+            </StatHelpText>
+          </Stat>
+        </StatGroup>
+      </SimpleGrid>
+    </ContentCentered>
   );
 }
 
 function Video({ launch }) {
   return (
-    <AspectRatioBox maxH="400px" ratio={1.7}>
-      <Box
-        as="iframe"
-        title={launch.mission_name}
-        src={`https://www.youtube.com/embed/${launch.links.youtube_id}`}
-        allowFullScreen
-      />
-    </AspectRatioBox>
+    <ContentCentered>
+      <AspectRatioBox maxH="400px" ratio={1.7}>
+        <Box
+          as="iframe"
+          title={launch.mission_name}
+          src={`https://www.youtube.com/embed/${launch.links.youtube_id}`}
+          allowFullScreen
+        />
+      </AspectRatioBox>
+    </ContentCentered>
   );
 }
 
 function Gallery({ images }) {
   return (
-    <SimpleGrid my="6" minChildWidth="350px" spacing="4">
-      {images.map((image) => (
-        <a href={image} key={image}>
-          <Image src={image.replace("_o.jpg", "_z.jpg")} />
-        </a>
-      ))}
-    </SimpleGrid>
+    <ContentCentered>
+      <SimpleGrid
+        my="6"
+        minChildWidth="350px"
+        spacing="4"
+        templateColumns="repeat(3, 1fr)"
+        templateRows="repeat(2, 1fr)"
+      >
+        {images.map((image) => (
+          <a href={image} key={image}>
+            <ImageDiv bcdUrl={image.replace("_o.jpg", "_z.jpg")}></ImageDiv>
+          </a>
+        ))}
+      </SimpleGrid>
+    </ContentCentered>
   );
+}
+
+function ImageOpened(imageUrl) {
+  return(<OpenedImage bcdUrl={imageUrl} />)
 }
